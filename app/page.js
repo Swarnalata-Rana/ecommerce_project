bb0d5f800994c3c31cf618252d9c4f37472587d7
Swarnalata Rana <swarnalata21@navgurukul.org>
2024-02-29 19:30:36 +0530

    ui-work1

--- app/page.js
+++ app/page.js
@@ -2,34 +2,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-// import Navbar from './Navbar';
-// import Footer from './Footer';
 import Slider from './Slider';
 import Place from './Place';
-// import Eloctronics from "./Eloctronics";
-// import Cloths from "./Cloths";
-// import Delevery_form from "./delivery/page";
-// import Successfully from "./successfully/page";
-// import Cart from "./cart/page";
-// import One_details from "./one-details/page";
 import ProductAll from "./ProductAll";
 
 const Page = () => {
   return (
     <>
-        {/* <Navbar/> */}
-        <Slider/>
-        <ProductAll/>
-        {/* <Cloths/>
-        <Eloctronics/> */}
-        <Place/>
-        {/* <Footer/> */}
-        {/* <Product/> */}
-        {/* <Pageshow/>f */}
-        {/* <Delevery_form/> */}
-        {/* <Successfully/> */}
-        {/* <Cart/> */}
-        {/* <One_details/> */}
+      <Slider />
+      <div className="container">
+        <ProductAll />
+      </div>
+      <Place />
+
     </>
   )
 }
